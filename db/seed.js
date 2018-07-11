e580f81b9c115a509dc283520ce31b3a91ae4000
Daniel Lousa <dlousa92@gmail.com>
2018-07-11 11:01:28 -0400

    add stock images to all sample posts

--- db/seed.js
+++ db/seed.js
@@ -21,7 +21,7 @@ User.find({}).remove(() => {
           title: 'First Day of Class',
           authorName: user._id,
           postString: 'Today was my first day at GA, I met my instructors and a lot of nice classmates I\'m really excited to start working on my projects!',
-          ImageURL: ''
+          ImageURL: 'https://images.pexels.com/photos/373488/pexels-photo-373488.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         }).then(memory => {
           user.memories.push(memory)
         }),
@@ -29,7 +29,7 @@ User.find({}).remove(() => {
           title: 'Stuck on Javascript',
           authorName: user._id,
           postString: 'I have no idea how Javascript works, I\'m so stuck!',
-          ImageURL: ''
+          ImageURL: 'https://images.pexels.com/photos/247791/pexels-photo-247791.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         }).then(memory => {
           user.memories.push(memory)
         }),
@@ -37,7 +37,7 @@ User.find({}).remove(() => {
           title: 'Group Project',
           authorName: user._id,
           postString: 'We just started our group project a few days ago! We\'re building a memory app and it\'s going to be a ton of fun',
-          ImageURL: ''
+          ImageURL: 'https://images.pexels.com/photos/1068523/pexels-photo-1068523.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         }).then(memory => {
           user.memories.push(memory)
         })
@@ -56,7 +56,7 @@ User.find({}).remove(() => {
           title: 'WOOOOOOO!!',
           authorName: user._id,
           postString: 'I\'m so excited and I just can\'t hide it',
-          ImageURL: ''
+          ImageURL: 'https://images.pexels.com/photos/119972/pexels-photo-119972.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         }).then(memory => {
           user.memories.push(memory)
         }),
@@ -64,7 +64,7 @@ User.find({}).remove(() => {
           title: 'I love Javascript',
           authorName: user._id,
           postString: 'I love everything I can do with Javascript, I just wanted to share with you all the project I\'m working on!',
-          ImageURL: ''
+          ImageURL: 'https://images.pexels.com/photos/574070/pexels-photo-574070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         }).then(memory => {
           user.memories.push(memory)
         })
@@ -83,7 +83,7 @@ User.find({}).remove(() => {
           title: 'Biking',
           authorName: user._id,
           postString: 'I biked my entire way accross North and South America, here\'s a picture from my trip',
-          ImageURL: ''
+          ImageURL: 'https://files.slack.com/files-pri/T2D6FU4JY-FBN67NBEV/image_from_ios.jpg'
         }).then(memory => {
           user.memories.push(memory)
         }),
@@ -91,7 +91,7 @@ User.find({}).remove(() => {
           title: 'Express.js',
           authorName: user._id,
           postString: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          ImageURL: ''
+          ImageURL: 'https://images.pexels.com/photos/265110/pexels-photo-265110.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         }).then(memory => {
           user.memories.push(memory)
         }),
@@ -107,7 +107,7 @@ User.find({}).remove(() => {
           title: 'Group Project!',
           authorName: user._id,
           postString: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat',
-          ImageURL: ''
+          ImageURL: 'https://images.pexels.com/photos/7097/people-coffee-tea-meeting.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         }).then(memory => {
           user.memories.push(memory)
         }),
@@ -115,7 +115,7 @@ User.find({}).remove(() => {
           title: 'Dinner with the boys',
           authorName: user._id,
           postString: 'Lorem ipsum dolor',
-          ImageURL: ''
+          ImageURL: 'https://images.pexels.com/photos/8313/food-eating-potatoes-beer-8313.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         }).then(memory => {
           user.memories.push(memory)
         })
