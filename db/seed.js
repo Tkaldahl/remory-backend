a8da3e9e04f550e574ecade3ce2b0fa2e0c8566f
Daniel Lousa <dlousa92@gmail.com>
2018-07-11 10:11:43 -0400

    set up basic seed file with users only

---
+++ db/seed.js
@@ -0,0 +1,39 @@
+const mongoose = require('./connection.js')
+const User = require('./User.js')
+const Memory = require('./Memory.js')
+const Comment = require('./Comment.js')
+mongoose.Promise = Promise
+
+User.find({}).remove(() => {
+  Memory.find({}).remove(() => {
+    let daniel = User.create({
+      email: 'daniel@sample.com',
+      password: '',
+      firstName: 'Daniel',
+      lastName: 'Lousa',
+      profPicture: ''
+    }).then((quote) => {
+      quote.save(err => console.log(err))
+    })
+
+    let taylor = User.create({
+      email: 'taylor@sample.com',
+      password: '',
+      firstName: 'Taylor',
+      lastName: 'Kaldahl',
+      profPicture: ''
+    }).then((quote) => {
+      quote.save(err => console.log(err))
+    })
+
+    let isaiah = User.create({
+      email: 'isaiah@sample.com',
+      password: '',
+      firstName: 'Isaiah',
+      lastName: 'Berg',
+      profPicture: ''
+    }).then((user) => {
+      user.save(err => console.log(err))
+    })
+  })
+})
