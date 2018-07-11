978f7b5c777599ab2553c6636ec1c1a51c14d1ab
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-07-11 11:41:25 -0400

    User model hotfix: updated bcrypt

--- db/User.js
+++ db/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('./connection.js')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
+// bcrypt is required to utilize the schema methods of .encrypt and .validPassword
 
 const User = new Schema({
   _id: Schema.Types.ObjectId,
@@ -9,7 +11,13 @@ const User = new Schema({
   lastName: String,
   profPicture: String,
   memories: [{ type: Schema.Types.ObjectId, ref: 'Memory' }]
-
 })
 
+User.methods.encrypt = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+}
+
+User.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.local.password)
+}
 module.exports = mongoose.model('User', User)
