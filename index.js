b68db45a7879ffc0737b6dd1736b1ad746e67842
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-07-11 21:52:34 -0400

    adjusted test console.log

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.listen(app.get('port'), () => {
 })
 
 app.listen(4000, () => {
-  console.log('✅: REMORY-backend test localhost:4000/users')
+  console.log('✅: REMORY-backend test localhost:4000/user and localhost:4000')
 })
 // here we set the port for development / heroku back end at 3001
 // we set the local "listening" port for localhost:4000
