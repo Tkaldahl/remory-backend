ccc11de0297286e56fb401e61e83601672bf18f3
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-07-11 21:51:15 -0400

    added routes for POST user/signup and GET all users

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const cors = require('cors')
 // cors overrides safety measures preventing external reqs to deployed apps
 const bodyParser = require('body-parser')
 // bodyParser takes care of JSON / HTTP request text transformations
+const bcrypt = require('bcrypt-nodejs')
+// bcrypt required to encrypt passwords upon POST / create requests
+const encryptPass = password =>
+  bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+// encryptPass(password) invoked on passwords passed via HTTP POST requests
+
 const User = require('./db/User.js')
 const Memory = require('./db/Memory.js')
 const Comment = require('./db/Comment.js')
@@ -19,7 +25,14 @@ app.use(express.static('public'))
 // note: created public directory for any desired static back end assets
 // example path: http://localhost:4000/style.css
 
+// creating default path for root of API
 app.get('/', (req, res) => {
+  res.render('Welcome to the Remory API!')
+})
+// responds with welcome message
+
+// creating route for GET all memories in database
+app.get('/memory', (req, res) => {
   Memory.find({})
     .then((memories) => {
       res.json(memories)
@@ -28,7 +41,31 @@ app.get('/', (req, res) => {
       console.log(err)
     })
 })
+// axios GET request at remory-api.herokuapp.com/memory returns: all memories
 
+// building route for creating user (POST) at /user
+app.post('/user', (req, res) => {
+  console.log(req.body)
+  User.create({
+    email: req.body.email,
+    password: encryptPass(req.body.password),
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    profPicture: req.body.profPicture
+  })
+    .then((user) => {
+      res.json(user)
+    })
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+// upon POST of form data at remory-api.herokuapp.com/user, new user in db
+
+// building route to see all users in db (useful for future search)
 app.get('/user', (req, res) => {
   User.find({})
     .then((users) => {
@@ -38,6 +75,9 @@ app.get('/user', (req, res) => {
       console.log(err)
     })
 })
+// upon GET request to remory-api.herokuapp.com/user, JSON response with all users
+
+
 // this is an example of the routing for a get request, at root, for Memory model
 // DATABASE does have other files besides Memory displayed in JSON response...
 
